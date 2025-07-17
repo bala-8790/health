972322fd146907da32db56d7cf8a6b321adc1633
Jonna BalaMahendra varma <srinivasa8790@gmail.com>
2025-07-17 11:04:16 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,16 @@
+import smtplib
+from email.message import EmailMessage
+
+def send_email(receiver_email, subject, content):
+    sender_email = "your_email@example.com"
+    password = "your_password"
+
+    msg = EmailMessage()
+    msg.set_content(content)
+    msg["Subject"] = subject
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(sender_email, password)
+        server.send_message(msg)
